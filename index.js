9af04e2c12128dfbd7cc3d7723752ff38074ce54
VergHar <vergsev@mail.ru>
2020-07-19 14:29:41 +0300

    add to js
    add to repo javascript

---
+++ index.js
@@ -0,0 +1,29 @@
+//alert(5+5)
+console.log("hello");
+console.log(5+6);
+console.log(7-6);
+console.log(2*6);
+console.log(8/4);
+console.log(8%2);
+console.log(Infinity+5);
+console.log(2.1+3.1);
+console.log(1+3*5);
+console.log((1+3)*5);
+console.log(2**5);
+var n=10;
+var myVariable=0;
+let x=1;
+const y=10;
+//Console.log(myVariable + x);
+console.log(myVariable);
+myVariable=2
+console.log(myVariable);
+console.log(x)
+x="hello"
+console.log(x);
+console.log(y)
+y=8;
+console.log(y);
+
+
+
